78cc033058fd9d59d5f4273ec31a0d029ae24ad5
Tej Chajed <tchajed@mit.edu>
2021-01-09 09:22:29 -0600

    Write encoding functions directly

--- src/dafny_go/encoding/encoding.go
+++ src/dafny_go/encoding/encoding.go
@@ -1,14 +1,15 @@
 package encoding
 
 import (
+	"encoding/binary"
+
 	"github.com/mit-pdos/dafny-jrnl/src/dafny_go/bytes"
-	"github.com/tchajed/goose/machine"
 )
 
 func UInt64Put(x uint64, off uint64, bs *bytes.Bytes) {
-	machine.UInt64Put(bs.Data[off:], x)
+	binary.LittleEndian.PutUint64(bs.Data[off:], x)
 }
 
 func UInt64Get(bs *bytes.Bytes, off uint64) uint64 {
-	return machine.UInt64Get(bs.Data[off:])
+	return binary.LittleEndian.Uint64(bs.Data[off:])
 }
