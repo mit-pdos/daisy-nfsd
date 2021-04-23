22564f2e5fd4bcbd86668d0183632faf1481b22a
Tej Chajed <tchajed@mit.edu>
2021-04-23 10:37:41 -0400

    Add a script to compile everything

---
+++ eval/build.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -eu
+
+# Compiles from Dafny to Go.
+#
+# The rest of the compilation is redundantly run by the benchmarking scripts,
+# but we run it here to cache the build.
+
+if [ ! -d "$DAFNY_NFSD_PATH" ]; then
+    echo "DAFNY_NFSD_PATH is unset" 1>&2
+    exit 1
+fi
+if [ ! -d "$GOOSE_NFSD_PATH" ]; then
+    echo "GOOSE_NFSD_PATH is unset" 1>&2
+    exit 1
+fi
+
+cd "$GOOSE_NFSD_PATH"
+go build ./cmd/fs-smallfile
+go build ./cmd/fs-largefile
+
+cd "$DAFNY_NFSD_PATH"
+make compile
+go build ./cmd/dafny-nfsd
+rm dafny-nfsd
