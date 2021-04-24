aa77550cc827cebf1ff04bd729f110ff7d7ad900
Tej Chajed <tchajed@mit.edu>
2021-04-23 22:48:56 -0400

    Add fixed scale figure plotting scripts

---
+++ eval/scale.plot
@@ -0,0 +1,27 @@
+set terminal png noenhanced size 1050,600
+set output "fig/scale.png"
+
+set auto x
+set yrange [0:*]
+set xtics 1
+set ylabel "files / sec"
+set format y '%.0s%c'
+set xlabel "\# clients"
+set key top left
+
+set style data line
+
+set style line 81 lt 0  # dashed
+set style line 81 lt rgb "#808080"  # grey
+set grid back linestyle 81
+
+set border 3 back linestyle 80
+set style line 1 lt rgb "#00A000" lw 2 pt 6
+set style line 2 lt rgb "#5060D0" lw 2 pt 1
+set style line 3 lt rgb "#A00000" lw 2 pt 2
+set style line 4 lt rgb "#F25900" lw 2 pt 9
+
+plot \
+  "data/dnfs.data" using 1:($2) with linespoints ls 1 title 'DafnyNFS', \
+  "data/gnfs.data" using 1:($2) with linespoints ls 1 title 'GoNFS', \
+  "data/linux-nfs.data" using 1:($2) with linespoints ls 2 title 'Linux NFS'
---
+++ eval/scale.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import sys
+import re
+
+import argparse
+import pandas as pd
+
+
+def parse_raw(lines):
+    fs = None
+    data = []
+
+    def get_bench_data(pattern, line):
+        m = re.match(pattern, line)
+        if m:
+            return {
+                "fs": fs,
+                "bench": m.group("bench"),
+                "val": float(m.group("val")),
+            }
+        return None
+
+    for line in lines:
+        m = re.match(r"""fs=(?P<fs>.*)""", line)
+        if m:
+            fs = m.group("fs")
+            continue
+        m = re.match(
+            r"""fs-smallfile: (?P<clients>\d*) (?P<val>[0-9.]*) file/sec""",
+            line,
+        )
+        if m:
+            data.append(
+                {
+                    "fs": fs,
+                    "clients": int(m.group("clients")),
+                    "throughput": float(m.group("val")),
+                }
+            )
+            continue
+        print("ignored line: " + line, end="", file=sys.stderr)
+
+    return pd.DataFrame.from_records(data)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("bench", type=argparse.FileType("r"))
+
+    args = parser.parse_args()
+
+    tidy_df = parse_raw(args.bench)
+    df = tidy_df.pivot_table(index="clients", columns="fs", values="throughput")
+    # propagate serial results forward
+    df = df.fillna(method="ffill")
+    with open("data/dnfs.data", "w") as f:
+        print(df["dnfs"].to_csv(sep="\t", header=False), end="", file=f)
+    with open("data/gnfs.data", "w") as f:
+        print(df["gonfs"].to_csv(sep="\t", header=False), end="", file=f)
+    with open("data/linux-nfs.data", "w") as f:
+        print(df["linux"].to_csv(sep="\t", header=False), end="", file=f)
