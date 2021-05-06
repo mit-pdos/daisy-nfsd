ec01f2a0ee5f0ce32b9bc965207685f33dce9299
Tej Chajed <tchajed@mit.edu>
2021-05-06 11:09:00 -0400

    Make it easier to tweak scale.sh path

--- eval/scale.sh
+++ eval/scale.sh
@@ -23,6 +23,10 @@ if [[ $# -gt 0 ]]; then
     threads="$1"
 fi
 
+# the path to store the disk file in (use this to run the benchmarks on a real
+# drive)
+disk_path="$HOME"
+
 cd "$GOOSE_NFSD_PATH"
 go build ./cmd/fs-smallfile
 go build ./cmd/fs-largefile
@@ -30,15 +34,15 @@ go build ./cmd/fs-largefile
 cd "$DAFNY_NFSD_PATH"
 info "DafnyNFS smallfile scalability"
 echo "fs=dnfs"
-./bench/run-dafny-nfs.sh -disk ~/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
+./bench/run-dafny-nfs.sh -disk "$disk_path"/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
 
 cd "$GOOSE_NFSD_PATH"
 echo 1>&2
 info "GoNFS smallfile scalability"
 echo "fs=gonfs"
-./bench/run-goose-nfs.sh -disk ~/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
+./bench/run-goose-nfs.sh -disk "$disk_path"/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
 
 echo 1>&2
 info "Linux smallfile scalability"
 echo "fs=linux"
-./bench/run-linux.sh     -disk ~/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
+./bench/run-linux.sh     -disk "$disk_path"/disk.img "$GOOSE_NFSD_PATH"/fs-smallfile -threads=$threads
