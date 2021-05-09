bebae3940d99b345212f75e6761bde526bcb64a2
Tej Chajed <tchajed@mit.edu>
2021-05-08 21:38:59 -0400

    Speed up start-dafny-nfs.sh
    Don't need to sleep till server is up, mount will wait until server is
    reachable.

--- bench/start-dafny-nfs.sh
+++ bench/start-dafny-nfs.sh
@@ -15,17 +15,21 @@ cd $DIR/..
 # make sure code is compiled in case it takes longer than 2s to build
 make --quiet compile
 go build ./cmd/dafny-nfsd && rm -f dafny-nfsd
-go run ./cmd/dafny-nfsd/ -debug=0 -disk /dev/shm/nfs.img "$@" > nfs.out 2>&1 &
-sleep 2
+go run ./cmd/dafny-nfsd/ -debug=0 -disk /dev/shm/nfs.img "$@" 1>nfs.out 2>&1 &
+# don't need nfsd to be running yet; mount will retry until it can connect
+sleep 0.5
 killall -0 dafny-nfsd # make sure server is running
 
 # mount options for Linux NFS client:
 #
 # vers=3 is the default but nice to be explicit
 #
-# nordirplus tells the client not to try READDIRPLUS (it will fall back to READDIR but always first try READDIRPLUS)
+# retry=1 tries to connect for up to a minute (default is 2)
+#
+# nordirplus tells the client not to try READDIRPLUS (it will fall back to
+# READDIR but always first try READDIRPLUS)
 #
 # nolock tells the client not to try to use the Network Lock Manager for
 # advisory locks since our server doesn't run one; instead, clients use local
 # locks which work fine if there's only one client
-sudo mount -t nfs -o vers=3,nordirplus,nolock localhost:/ /mnt/nfs
+sudo mount -t nfs -o vers=3,retry=1,nordirplus,nolock localhost:/ /mnt/nfs
