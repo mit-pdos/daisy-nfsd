22b50d5a4f5b560cb625acfa91983c49750dbb21
Tej Chajed <tchajed@mit.edu>
2021-05-19 10:06:43 -0400

    Change some stuff for debugging

--- bench/start-dafny-nfs.sh
+++ bench/start-dafny-nfs.sh
@@ -17,40 +17,39 @@ nfs_mount_opts=""
 nfs_mount_path="/mnt/nfs"
 extra_args=()
 while [[ "$#" -gt 0 ]]; do
-    case "$1" in
-    -disk)
-        shift
-        disk_path="$1"
-        shift
-        ;;
-    -nfs-mount-opts)
-        shift
-        nfs_mount_opts="$1"
-        shift
-        ;;
-    -mount-path)
-        shift
-        nfs_mount_path="$1"
-        shift
-        ;;
-    -*=*)
-        extra_args+=("$1")
-        shift
-        ;;
-    -*)
-        extra_args+=("$1" "$2")
-        shift
-        shift
-        ;;
-    esac
+	case "$1" in
+	-disk)
+		shift
+		disk_path="$1"
+		shift
+		;;
+	-nfs-mount-opts)
+		shift
+		nfs_mount_opts="$1"
+		shift
+		;;
+	-mount-path)
+		shift
+		nfs_mount_path="$1"
+		shift
+		;;
+	-*=*)
+		extra_args+=("$1")
+		shift
+		;;
+	-*)
+		extra_args+=("$1" "$2")
+		shift
+		shift
+		;;
+	esac
 done
 
 # make sure code is compiled in case it takes longer than 2s to build
 make --quiet compile
 go build ./cmd/dafny-nfsd
 ./dafny-nfsd -debug=0 -disk "$disk_path" "${extra_args[@]}" 1>nfs.out 2>&1 &
-# don't need nfsd to be running yet; mount will retry until it can connect
-sleep 0.5
+sleep 2
 killall -0 dafny-nfsd # make sure server is running
 
 # mount options for Linux NFS client:
@@ -65,7 +64,7 @@ killall -0 dafny-nfsd # make sure server is running
 # locks which work fine if there's only one client
 _nfs_mount="vers=3,nordirplus,nolock"
 if [ -n "$nfs_mount_opts" ]; then
-    _nfs_mount="vers=3,nordirplus,$nfs_mount_opts"
+	_nfs_mount="vers=3,nordirplus,$nfs_mount_opts"
 fi
 
 sudo mount -t nfs -o ${_nfs_mount} localhost:/ "$nfs_mount_path"
