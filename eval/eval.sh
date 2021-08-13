6f58b5574db16b4512722ac74c9b384f33bab0be
Tej Chajed <tchajed@mit.edu>
2021-08-13 12:36:45 -0400

    Wrap up new evaluation flow including plotting

--- eval/eval.sh
+++ eval/eval.sh
@@ -4,9 +4,17 @@ set -eu
 # run the entire eval
 
 DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
-cd "$DIR"
+cd "$DIR/.."
+
+iters=1
+if [ "$#" -gt 0 ]; then
+    iters="$1"
+fi
 
 #./loc.py      | tee data/lines-of-code.txt
-./bench.sh | tee data/bench-raw.txt
-./scale.sh 10 | tee data/scale-raw.txt
-./plot.sh
+go run ./cmd/daisy-eval -dir eval/data -iters "$iters" bench
+./eval.py -i data bench
+go run ./cmd/daisy-eval -dir eval/data -iters "$iters" scale
+./eval.py -i data scale
+gnuplot bench.plot
+gnuplot scale.plot
--- eval/plot.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-./bench.py data/bench-raw.txt
-./scale.py data/scale-raw.txt
-gnuplot bench.plot
-gnuplot scale.plot
