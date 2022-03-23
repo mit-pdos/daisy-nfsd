cc6af25204d9961f19a6bc143eb8193f787a429f
Tej Chajed <tchajed@mit.edu>
2022-03-23 17:15:44 -0400

    Update default benchmarks in aws/eval.sh

--- eval/aws/eval.sh
+++ eval/aws/eval.sh
@@ -3,11 +3,17 @@
 set -eu
 
 cd ~/daisy-nfsd
+
 go build ./cmd/daisy-eval
 mkdir -p eval/data/nvme
 sudo chown "$USER" /dev/nvme1n1
-time ./daisy-eval --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 bench
-time ./daisy-eval --dir eval/data --disk "/dev/shm/disk.img" --iters 10 bench
-time ./daisy-eval --dir eval/data/nvme --disk /dev/nvme1n1 --iters 1 scale --benchtime=5s --threads 36
-time ./daisy-eval --filesystems durability --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 --wait 3s largefile
-time ./daisy-eval --filesystems durability --dir eval/data --disk "/dev/shm/disk.img" --iters 10 --wait 3s largefile
+
+time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 bench
+time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk /dev/nvme1n1 --iters 3 scale --benchtime=10s --threads 36
+time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data --disk "/dev/shm/disk.img" --iters 10 bench
+
+#time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data--disk "/dev/shm/disk.img" --iters 3 scale --benchtime=10s --threads 36
+
+# some other things to try
+#time ./daisy-eval --filesystems durability --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 --wait 3s largefile
+#time ./daisy-eval --filesystems durability --dir eval/data --disk "/dev/shm/disk.img" --iters 10 --wait 3s largefile
