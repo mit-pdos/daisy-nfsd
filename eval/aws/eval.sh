5c44fa29a3074628eb743ba39b244fcfcc3c55cf
Tej Chajed <tchajed@mit.edu>
2022-04-29 09:54:26 -0400

    Add commands to run extended-bench to aws/eval.sh

--- eval/aws/eval.sh
+++ eval/aws/eval.sh
@@ -8,8 +8,8 @@ go build ./cmd/daisy-eval
 mkdir -p eval/data/nvme
 sudo chown "$USER" /dev/nvme1n1
 
-./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk /dev/nvme1n1 --iters 1 bench
-rm eval/data/nvme/*
+time ./daisy-eval --filesystems extended --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 extended-bench --benchtime=30s --par=25
+time ./daisy-eval --filesystems extended --dir eval/data --disk /dev/shm/disk.img --iters 10 extended-bench
 
 time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk /dev/nvme1n1 --iters 10 bench
 time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk /dev/nvme1n1 --iters 3 scale --benchtime=10s --threads 36
