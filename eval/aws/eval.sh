68e26d4c1986c43cec5f1e1532f345ca77ca1be4
Tej Chajed <tchajed@mit.edu>
2022-05-04 11:29:55 -0400

    Change scale to 1 iteration of 30s

--- eval/aws/eval.sh
+++ eval/aws/eval.sh
@@ -17,7 +17,7 @@ time ./daisy-eval --filesystems extended --dir eval/data/nvme --disk "$nvme" --i
 time ./daisy-eval --filesystems extended --dir eval/data --disk "$mem" --iters 5 extended-bench --benchtime=20s --par=25
 
 time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data --disk "$mem" --iters 10 bench --benchtime=20s
-time ./daisy-eval --filesystems "$scale_fss" --dir eval/data/nvme --disk "$nvme" --iters 5 scale --benchtime=10s --threads 36
+time ./daisy-eval --filesystems "$scale_fss" --dir eval/data/nvme --disk "$nvme" --iters 1 scale --benchtime=30s --threads 36
 
 #time ./daisy-eval --filesystems daisy-nfsd,linux --dir eval/data/nvme --disk "$nvme" --iters 3 scale --benchtime=10s --threads 36
 
