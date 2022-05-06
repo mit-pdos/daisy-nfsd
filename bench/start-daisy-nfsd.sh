8b031baf7f183f4569b1cdc0bcd90094f972e0d0
Tej Chajed <tchajed@mit.edu>
2022-05-06 10:48:50 -0400

    Make start-daisy-nfsd.sh compatible with macOS

--- bench/start-daisy-nfsd.sh
+++ bench/start-daisy-nfsd.sh
@@ -53,7 +53,7 @@ done
 
 make --quiet compile
 go build ./cmd/daisy-nfsd
-dd status=none if=/dev/zero of="$disk_path" bs=4k count=$((size_mb * 1024 / 4))
+dd if=/dev/zero of="$disk_path" bs=4k count=$((size_mb * 1024 / 4)) 1>/dev/null 2>&1
 ./daisy-nfsd -debug=0 -disk "$disk_path" -size "$size_mb" "${extra_args[@]}" 1>nfs.out 2>&1 &
 sleep 2
 killall -0 daisy-nfsd       # make sure server is running
