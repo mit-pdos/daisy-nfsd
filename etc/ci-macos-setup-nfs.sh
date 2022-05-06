3ca18fec7158845c1483275384c10a4bb2421f50
Tej Chajed <tchajed@mit.edu>
2022-05-06 13:24:07 -0400

    Try to fix the macOS 12 CI issue

--- etc/ci-macos-setup-nfs.sh
+++ etc/ci-macos-setup-nfs.sh
@@ -6,4 +6,8 @@ sudo launchctl start com.apple.rpcbind
 # this is not needed as long as you mount with -o locallocks
 sudo launchctl start com.apple.lockd
 
+sleep 2
+
+sudo launchctl list | grep rpcbind
+
 mkdir /tmp/nfs
